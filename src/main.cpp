82514b7092398e8722af2afcabc6265efa6d11c0
Cerwym <1760289+Cerwym@users.noreply.github.com>
2026-02-27 08:49:33 +0000

    fix: reset frame_skip to 0 when loading a new level

--- src/main.cpp
+++ src/main.cpp
@@ -3984,6 +3984,7 @@ void game_loop(void)
       }
       LbScreenClear(0);
       LbScreenSwap();
+      game.frame_skip = 0;
       keeper_gameplay_loop();
       set_pointer_graphic_none();
       LbScreenClear(0);
