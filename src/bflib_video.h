244b2cf12755669f6fabdd0e129ae952d73a4002
Cerwym <1760289+Cerwym@users.noreply.github.com>
2026-05-25 15:43:16 +0100

    build: fix abs(long), missing declarations, and overlapping strcpy for Linux CI

--- src/bflib_video.h
+++ src/bflib_video.h
@@ -302,6 +302,9 @@ TbScreenCoord LbGraphicsScreenHeight(void);
 unsigned short LbGetCurrentDisplayIndex();
 
 TbPixel LbPaletteFindColour(const unsigned char *pal, unsigned char r, unsigned char g, unsigned char b);
+TbResult LbScreenWaitVbi(void);
+TbResult LbPaletteSet(unsigned char *palette);
+TbResult LbPaletteGet(unsigned char *palette);
 TbResult LbPaletteDataFillBlack(unsigned char *palette);
 TbResult LbPaletteDataFillWhite(unsigned char *palette);
 
