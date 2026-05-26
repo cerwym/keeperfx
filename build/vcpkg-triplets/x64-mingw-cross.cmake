650bf543b23dc5e5258040e5950863cf681e06b0
Cerwym <1760289+Cerwym@users.noreply.github.com>
2026-05-26 11:48:08 +0100

    vcpkg: fix MinGW triplet system name and add missing build tools to Docker images
    - Set VCPKG_CMAKE_SYSTEM_NAME=MinGW (was Windows) in both MinGW cross triplets
      so vcpkg routes openssl and other ports to the correct MinGW build path
    - Add autoconf/automake/libtool/perl to mingw64 and mingw32 Docker images
      (needed by openssl configure and other autoconf-based vcpkg ports)
    - Add autoconf/automake/libtool to linux Docker image
      (needed by alsa and other configure-based vcpkg ports)

--- build/vcpkg-triplets/x64-mingw-cross.cmake
+++ build/vcpkg-triplets/x64-mingw-cross.cmake
@@ -7,7 +7,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_CMAKE_SYSTEM_NAME Windows)
+set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
 
 get_filename_component(_triplet_dir "${CMAKE_CURRENT_LIST_FILE}" DIRECTORY)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${_triplet_dir}/../cmake/toolchains/mingw64-posix.cmake")
