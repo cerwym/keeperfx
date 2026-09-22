312d1f1600d700ed4325d15cfb045188cb2a566f
Cerwym <1760289+Cerwym@users.noreply.github.com>
2026-09-22 06:52:00 +0100

    test: add a second Game field

--- src/game_legacy.h
+++ src/game_legacy.h
@@ -291,6 +291,7 @@ struct Game {
     ThingIndex unsynced_free_things_count;
     GameTurn play_gameturn;
     GameTurn pckt_gameturn;
+    int32_t save_check_demo_field2;
     /** Synchronized random seed. used for game actions, as it's always identical for clients of network game. */
     uint32_t action_random_seed;
     uint32_t ai_random_seed;
